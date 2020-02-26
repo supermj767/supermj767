f9e04892a751c6919278950381b3ce6b7bfe477e
supermj767 <4mjs0217@gmail.com>
2020-02-26 01:35:00 -0600

    New

---
+++ src/main/java/net/mcreator/random/MCreatorItem.java
@@ -0,0 +1,59 @@
+package net.mcreator.random;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+@Elementsrandom.ModElement.Tag
+public class MCreatorItem extends Elementsrandom.ModElement {
+	@GameRegistry.ObjectHolder("random:item")
+	public static final Item block = null;
+
+	public MCreatorItem(Elementsrandom instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("random:item", "inventory"));
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(0);
+			maxStackSize = 64;
+			setUnlocalizedName("item");
+			setRegistryName("item");
+			setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+	}
+}
